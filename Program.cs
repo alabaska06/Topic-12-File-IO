59e4455873aadb91fc285c0cd7499de8fce84a00
Alanna Smith <smitalan240@gmail.com>
2024-03-18 13:10:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_12_File_IO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
